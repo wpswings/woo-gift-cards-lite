709c237483405bbfdb92ee281f0cb55d08e72bbc
pradeepsinghbisht-cedcoss <pradeepsinghbisht@makewebbetter.com>
2021-05-06 11:33:19 +0530

    tested upto WP 5.7.1 and WC 5.2.2

--- woocommerce_gift_cards_lite.php
+++ woocommerce_gift_cards_lite.php
@@ -21,8 +21,8 @@
  * License:           GPL-3.0+
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:       woo-gift-cards-lite
- * Tested up to:      5.6
- * WC tested up to:   5.0
+ * Tested up to:      5.7.1
+ * WC tested up to:   5.2.2
  * Domain Path:       /languages
  */
 
