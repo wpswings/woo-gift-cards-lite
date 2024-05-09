1f90a8095793b6114218af27ddefe9f571c0014d
pradeepsinghbisht-cedcoss <pradeepsinghbisht@makewebbbetter.com>
2024-05-09 12:40:31 +0530

    version updated

--- includes/class-woocommerce-gift-cards-lite.php
+++ includes/class-woocommerce-gift-cards-lite.php
@@ -72,7 +72,7 @@ class Woocommerce_Gift_Cards_Lite {
 		if ( defined( 'WPS_WGC_VERSION' ) ) {
 			$this->version = WPS_WGC_VERSION;
 		} else {
-			$this->version = '2.6.10';
+			$this->version = '2.7.0';
 		}
 		$this->plugin_name = 'woo-gift-cards-lite';
 
--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Requires at least: 5.1.0
 Tested up to: 6.4.2
 WC requires at least: 5.5.0
 WC tested up to: 8.4.0
-Stable tag:  2.6.10
+Stable tag:  2.7.0
 Requires PHP: 5.6
 License: GPL-3.0+
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
