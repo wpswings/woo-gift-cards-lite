4e19ef42357e537f5724755c2b3ef0afcc8aef87
pradeepsinghbisht-cedcoss <pradeepsinghbisht@makewebbetter.com>
2025-02-12 16:03:16 +0530

    check_balance_page_js

--- public/class-woocommerce-gift-cards-lite-public.php
+++ public/class-woocommerce-gift-cards-lite-public.php
@@ -240,9 +240,9 @@ class Woocommerce_Gift_Cards_Lite_Public {
 				'empty_msg'       => esc_html__( 'Fields cannot be empty!', 'woo-gift-cards-lite' ), // PAR Compatibility.
 			);
 
-			wp_register_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/woocommerce_gifr_cards_lite_check_balance.js', array( 'jquery' ), $this->version, true );
-			wp_localize_script( $this->plugin_name, 'wps_wgm_check_balance', $wps_wgm_check_balance );
-			wp_enqueue_script( $this->plugin_name );
+			wp_register_script( $this->plugin_name . 'check_balance_page', plugin_dir_url( __FILE__ ) . 'js/woocommerce_gifr_cards_lite_check_balance.js', array( 'jquery' ), $this->version, true );
+			wp_localize_script( $this->plugin_name . 'check_balance_page', 'wps_wgm_check_balance', $wps_wgm_check_balance );
+			wp_enqueue_script( $this->plugin_name . 'check_balance_page' );
 		}
 	}
 	/**
