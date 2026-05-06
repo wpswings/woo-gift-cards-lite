5677a65fb844bd1f0a88f27f0d0730fecdbb487c
pradeepsinghbisht-cedcoss <pradeepsinghbisht@makewebbetter.com>
2026-05-06 18:51:57 +0530

    fix

--- admin/js/wpswings-onboarding-admin.js
+++ admin/js/wpswings-onboarding-admin.js
@@ -7,10 +7,12 @@ jQuery(document).ready( function($) {
 
 	var deactivate_url = '';
 
-	// Add Select2.
-	jQuery( '.on-boarding-select2' ).select2({
-		placeholder : 'Select All Suitable Options...',
-	});
+	// Add Select2 when the library is available on the current admin screen.
+	if ( jQuery.fn.select2 ) {
+		jQuery( '.on-boarding-select2' ).select2({
+			placeholder : 'Select All Suitable Options...',
+		});
+	}
 
 	// Add Deactivation id to all deactivation links.
 	embed_id_to_deactivation_urls();
@@ -47,6 +49,7 @@ jQuery(document).ready( function($) {
 
 	/* Skip and deactivate. */
 	jQuery( document ).on( 'click','.wps-deactivation-no_thanks',function(e){
+		e.preventDefault();
 
 		window.location.replace( deactivate_url );
 		wps_hide_onboard_popup();
@@ -117,29 +120,41 @@ jQuery(document).ready( function($) {
 
 	/* Apply deactivate in all the WPS plugins. */
 	function add_deactivate_slugs_callback( all_slugs ) {
-		
-		for ( var i = all_slugs.length - 1; i >= 0; i-- ) {
+		all_slugs = jQuery.isArray( all_slugs ) ? all_slugs : [];
 
-			jQuery( document ).on( 'click', '#deactivate-' + all_slugs[i] ,function(e){
+		jQuery( document ).on( 'click', 'a[href*="action=deactivate"]', function(e) {
+			var slug = jQuery( this ).closest( 'tr' ).attr( 'data-slug' );
+			var href = jQuery( this ).attr( 'href' ) || '';
+			var is_supported_slug = -1 < jQuery.inArray( slug, all_slugs );
+			var is_giftcard_deactivation = -1 < href.indexOf( 'woo-gift-cards-lite%2Fwoocommerce_gift_cards_lite.php' ) || -1 < href.indexOf( 'woo-gift-cards-lite/woocommerce_gift_cards_lite.php' );
 
-				e.preventDefault();
-				deactivate_url = jQuery( this ).attr( 'href' );
-				plugin_name = jQuery( this ).attr( 'aria-label' );
-				jQuery( '#plugin-name' ).val( plugin_name.replace( 'Deactivate ', '' ) );
-				plugin_name = plugin_name.replace( 'Deactivate ', '' );
-				jQuery( '#plugin-name' ).val( plugin_name );
-				jQuery( '.wps-on-boarding-heading' ).text( plugin_name + ' Feedback' );
-				var placeholder = jQuery( '#deactivation-reason-text' ).attr( 'placeholder' );
-				jQuery( '#deactivation-reason-text' ).attr( 'placeholder', placeholder.replace( '{plugin-name}', plugin_name ) );
-				wps_show_onboard_popup();
-			});
-		}
+			if ( ! is_supported_slug && ! is_giftcard_deactivation ) {
+				return;
+			}
+
+			e.preventDefault();
+			deactivate_url = href;
+			var plugin_name = jQuery( this ).attr( 'aria-label' ) || '';
+			plugin_name = plugin_name.replace( 'Deactivate ', '' );
+
+			if ( ! plugin_name ) {
+				plugin_name = jQuery( this ).closest( 'tr' ).find( '.plugin-title strong' ).first().text();
+			}
+
+			jQuery( '#plugin-name' ).val( plugin_name );
+			jQuery( '.wps-on-boarding-heading' ).text( plugin_name + ' Feedback' );
+
+			var placeholder = jQuery( '#deactivation-reason-text' ).attr( 'placeholder' ) || '';
+			jQuery( '#deactivation-reason-text' ).attr( 'placeholder', placeholder.replace( '{plugin-name}', plugin_name ) );
+			wps_show_onboard_popup();
+		});
 	}
 
 	/* Add deactivate id in all the plugins links. */
 	function embed_id_to_deactivation_urls() {
 		jQuery( 'a' ).each(function(){
-		    if ( 'Deactivate' == jQuery(this).text() && 0 < jQuery(this).attr( 'href' ).search( 'action=deactivate' ) ) {
+			var href = jQuery(this).attr( 'href' ) || '';
+		    if ( 'Deactivate' == jQuery.trim( jQuery(this).text() ) && 0 < href.search( 'action=deactivate' ) ) {
 		    	if( 'undefined' == typeof jQuery(this).attr( 'id' ) ) {
 			    	var slug = jQuery(this).closest( 'tr' ).attr( 'data-slug' );
 			    	jQuery(this).attr( 'id', 'deactivate-' + slug );
